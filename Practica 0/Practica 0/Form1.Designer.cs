f850fe018177047452f90b70d2b37928041583d2
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-02-05 18:11:29 +0100

    Add files via upload

---
+++ Practica 0/Practica 0/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Practica_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnombre = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnombre
+            // 
+            this.btnombre.Location = new System.Drawing.Point(60, 183);
+            this.btnombre.Name = "btnombre";
+            this.btnombre.Size = new System.Drawing.Size(288, 80);
+            this.btnombre.TabIndex = 0;
+            this.btnombre.Text = "Saludo";
+            this.btnombre.UseVisualStyleBackColor = true;
+            this.btnombre.Click += new System.EventHandler(this.btnombre_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnsalir.Location = new System.Drawing.Point(384, 180);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(288, 87);
+            this.btnsalir.TabIndex = 1;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = false;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(156, 131);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(368, 26);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(152, 94);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(65, 20);
+            this.lblnombre.TabIndex = 3;
+            this.lblnombre.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 369);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnombre);
+            this.Name = "Form1";
+            this.Text = "Práctica 0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnombre;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblnombre;
+    }
+}
+
