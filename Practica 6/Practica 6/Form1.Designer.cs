a25592b025c71276dbc3187d4bbebfc2f08fbf11
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-04-19 11:45:16 +0200

    Add files via upload

---
+++ Practica 6/Practica 6/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Practica_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(47, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(707, 264);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(506, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cargar XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Modificar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(323, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Guardar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Practica 6/Practica 6/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Practica_6
+{
+    public partial class Form1 : Form
+    {
+        private XmlDocument xmlDoc;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            {
+                // Crear un OpenFileDialog para seleccionar el archivo XML
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Archivos XML (*.xml)|*.xml";
+                openFileDialog.Title = "Seleccionar archivo XML";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string xmlFilePath = openFileDialog.FileName;
+                    xmlDoc.Load(xmlFilePath);
+
+                    XmlNodeList titulo = xmlDoc.GetElementsByTagName("titulo");
+                    if (titulo.Count > 0)
+                    {
+                        this.Text = titulo[0].InnerText;
+                    }
+
+                    XmlNodeList personas = xmlDoc.GetElementsByTagName("persona");
+                    foreach (XmlElement personaNode in personas)
+                    {
+                        string nombre = GetNodeInnerText(personaNode, "nombre");
+                        string apellido1 = GetNodeInnerText(personaNode, "apellido1");
+                        string apellido2 = GetNodeInnerText(personaNode, "apellido2");
+
+                        listBox1.Items.Add(nombre);
+                        listBox1.Items.Add("\t" + apellido1);
+                        listBox1.Items.Add("\t" + apellido2);
+
+                        XmlNodeList telefonos = personaNode.GetElementsByTagName("telefono");
+                        foreach (XmlElement telefonoNode in telefonos)
+                        {
+                            listBox1.Items.Add("\t" + telefonoNode.InnerText);
+                        }
+
+                        XmlNodeList emails = personaNode.GetElementsByTagName("email");
+                        foreach (XmlElement emailNode in emails)
+                        {
+                            listBox1.Items.Add("\t" + emailNode.InnerText);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el archivo XML: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Cerrar el documento XML después de cargar
+                xmlDoc = null; // Liberar la referencia al documento XML
+            }
+        }
+
+        private string GetNodeInnerText(XmlElement parentNode, string nodeName)
+        {
+            XmlNodeList nodeList = parentNode.GetElementsByTagName(nodeName);
+            if (nodeList.Count > 0)
+            {
+                return nodeList[0].InnerText;
+            }
+            return string.Empty;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                // Obtener el texto completo del elemento seleccionado en el ListBox
+                string selectedText = listBox1.SelectedItem.ToString();
+
+                // Buscar el índice de la primera tabulación en el texto
+                int tabIndex = selectedText.IndexOf('\t');
+
+                if (tabIndex != -1 && tabIndex < selectedText.Length - 1)
+                {
+                    // Extraer el apellido, que está después de la primera tabulación
+                    string apellidoSeleccionado = selectedText.Substring(tabIndex + 1).Trim();
+
+                    try
+                    {
+                        // Ruta del archivo XML (puedes cambiar la ruta según sea necesario)
+                        string xmlFilePath = @"C:\Users\enriq\Downloads\ejemplo_2.xml";
+                        XmlDocument xmlDoc = new XmlDocument();
+                        xmlDoc.Load(xmlFilePath);
+
+                        // Obtener la lista de nodos <persona> del archivo XML
+                        XmlNodeList personas = xmlDoc.SelectNodes("//persona");
+
+                        foreach (XmlNode personaNode in personas)
+                        {
+                            XmlNode apellido1Node = personaNode.SelectSingleNode("apellido1");
+
+                            if (apellido1Node != null && apellido1Node.InnerText.Equals(apellidoSeleccionado, StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Modificar el apellido1 en el XML con el texto del TextBox
+                                apellido1Node.InnerText = textBox1.Text;
+                            }
+                        }
+
+                        // Guardar los cambios en el archivo XML
+                        xmlDoc.Save(xmlFilePath);
+
+                        // Limpiar el ListBox y volver a cargar los datos actualizados
+                        listBox1.Items.Clear();
+                        button1_Click(sender, e); // Recargar los datos en el ListBox
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al modificar el archivo XML: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El formato del elemento seleccionado en el ListBox no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un elemento del ListBox para modificar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Ruta del archivo XML (puedes cambiar la ruta según sea necesario)
+                string xmlFilePath = @"C:\Users\enriq\Downloads\ejemplo_2.xml";
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlFilePath);
+
+                // Obtener la lista de nodos <persona> del archivo XML
+                XmlNodeList personas = xmlDoc.SelectNodes("//persona");
+
+                foreach (XmlNode personaNode in personas)
+                {
+                    XmlNode apellido1Node = personaNode.SelectSingleNode("apellido1");
+
+                    if (apellido1Node != null)
+                    {
+                        // Modificar el apellido1 en el XML con el texto del TextBox
+                        apellido1Node.InnerText = textBox1.Text;
+                    }
+                }
+
+                // Guardar los cambios en el archivo XML
+                xmlDoc.Save(xmlFilePath);
+
+                MessageBox.Show("Archivo XML guardado correctamente.", "Guardar XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar el archivo XML: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Cerrar el documento XML después de guardar
+                xmlDoc = null; // Liberar la referencia al documento XML
+            }
+        }
+    }
+}
