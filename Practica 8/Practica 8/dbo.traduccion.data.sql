1da66b80c6131c9b55529b1a8a0b6c8c50ffebb0
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-05-28 16:12:12 +0200

    Add files via upload

---
+++ Practica 8/Practica 8/dbo.traduccion.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Adiós               ', N'Bye                 ')
+INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Gato                ', N'Cat                 ')
+INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Hola                ', N'Hi                  ')
+INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Perro               ', N'Dog                 ')
