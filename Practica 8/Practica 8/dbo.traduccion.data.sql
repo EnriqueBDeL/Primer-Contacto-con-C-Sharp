3ae9d508ff9e179c3fdea2b67c2981b4bd741e59
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-05-28 16:12:31 +0200

    Delete Practica 8/Practica 8 directory

--- Practica 8/Practica 8/dbo.traduccion.data.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Adiós               ', N'Bye                 ')
-INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Gato                ', N'Cat                 ')
-INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Hola                ', N'Hi                  ')
-INSERT INTO [dbo].[traduccion] ([esp], [ing]) VALUES (N'Perro               ', N'Dog                 ')
